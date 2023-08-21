b1b0b3bf76c5aeabd6c2ed7f6479b34cb5a85341
hayeong <hychoi318@cttd.co.kr>
2023-08-21 15:32:37 +0900

    test: other device test

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Git Test component!
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
